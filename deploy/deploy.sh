2c95768412fcda1cd0ae5c0abdf9ec2112ea97b0
Ferran Llop <contacto@ferranllop.com>
2022-04-24 12:05:04 +0200

    Force symlink to avoid error message when it already exists

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,5 @@ BUILD_SOURCE=../src
 BUILD_DESTINATION=./apache/build
 rm -rf $BUILD_DESTINATION
 cp -r $BUILD_SOURCE $BUILD_DESTINATION
-ln -s ../.env .env
-docker-compose up -d --build
+ln --symbolic --force ../.env .env
+docker-compose up --detach --build
