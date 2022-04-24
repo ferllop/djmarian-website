d37b5be1f030cdcf6f5d2fab3358facebf316726
Ferran Llop <contacto@ferranllop.com>
2022-04-24 11:57:49 +0200

    Move deploy script into deploy folder

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ln -s ../.env ./deploy/.env
-cd deploy
-./git-post-receive-hook.sh
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,4 +3,5 @@ BUILD_SOURCE=../src
 BUILD_DESTINATION=./apache/build
 rm -rf $BUILD_DESTINATION
 cp -r $BUILD_SOURCE $BUILD_DESTINATION
+ln -s ../.env .env
 docker-compose up -d --build
