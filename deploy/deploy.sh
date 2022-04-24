73f5b9bc30e886024517f21dde8255aecf0a3892
Ferran Llop <contacto@ferranllop.com>
2022-04-24 12:50:11 +0200

    Make eleventy to build the website

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-BUILD_SOURCE=../src
-BUILD_DESTINATION=./apache/build
+BUILD_SOURCE=$(pwd)/../src
+BUILD_DESTINATION=$(pwd)/apache/build
 rm -rf $BUILD_DESTINATION
-cp -r $BUILD_SOURCE $BUILD_DESTINATION
+mkdir $BUILD_DESTINATION
+docker run -v $BUILD_SOURCE:/code -v $BUILD_DESTINATION:/build node:16.14.2-alpine /bin/sh -c "npm i -g @11ty/eleventy; eleventy --input=/code --output=/build"
 ln --symbolic --force ../.env .env
 docker-compose up --detach --build
