242b2bac33ed5d97fcb0591551ddb2d62359fa22
Ferran Llop <contacto@ferranllop.com>
2022-04-25 19:52:16 +0200

    Make apache container to serve files from eleventy container output

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-BUILD_SOURCE=$(pwd)/../src
-BUILD_DESTINATION=$(pwd)/apache/build
+BUILD_DESTINATION=../build
 rm -rf $BUILD_DESTINATION
 mkdir $BUILD_DESTINATION
-docker run -v $BUILD_SOURCE:/code -v $BUILD_DESTINATION:/build node:16.14.2-alpine /bin/sh -c "npm i -g @11ty/eleventy; eleventy --input=/code --output=/build"
 ln --symbolic --force ../.env .env
-docker-compose up --detach --build
+docker-compose up --detach --build --force
+docker-compose run eleventy eleventy --input=/source --output=/build
