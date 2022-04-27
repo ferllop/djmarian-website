9111bda5655027e0eb2b5e951e4f90d73b830872
Ferran Llop <contacto@ferranllop.com>
2022-04-27 20:08:56 +0200

    Add options for vimeo embeds

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
 const embedVimeo = require("eleventy-plugin-vimeo-embed")
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(embedVimeo)
+  eleventyConfig.addPlugin(embedVimeo, {
+      with: 300,
+      height: 150,
+      frameborder: 0,
+      allowFullscreen: true,
+      dnt: true,
+      embedClass: 'eleventy-plugin-vimeo-embed'
+  })
 
   return {
     dir: {
