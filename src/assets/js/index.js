53e8289f0b84573ff189212d6d5a39eac6adec47
Ferran Llop <contacto@ferranllop.com>
2022-05-11 19:08:07 +0200

    Convert video from to webcomponent

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,9 @@
 import { MainHeader } from './Header.js'
 import { MainFooter } from './Footer.js'
 import { Review } from './Review.js'
+import { VideoWebComponent } from './Video.js'
 
 customElements.define('djm-main-header', MainHeader)
 customElements.define('djm-main-footer', MainFooter)
 customElements.define('djm-review', Review)
+customElements.define('djm-video', VideoWebComponent)
