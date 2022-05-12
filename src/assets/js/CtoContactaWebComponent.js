629f77aca4642836483c4a5e4400dcf1d51e72bc
Ferran Llop <contacto@ferranllop.com>
2022-05-12 19:38:07 +0200

    Style contact CTO

--- src/assets/js/CtoContactaWebComponent.js
+++ src/assets/js/CtoContactaWebComponent.js
@@ -1,12 +1,25 @@
+const style = `
+a {
+    display: inline-block;
+    margin-top: 1.5em;
+    margin-bottom: 1.5em;
+    padding: 6px 24px;
+    background-color: #bc3315;
+    border: 1px solid #942911;
+    border-radius: 6px;
+    color: #ffffff;
+    line-height: 1.75;
+    text-decoration: none;
+    cursor: pointer;
+}
+`
 const template = document.createElement('template')
 template.innerHTML = `
-<p class="has-text-align-center">
-    <span class="red-button">
+<style>${style}</style>
         <a href="/contacto/">
             <strong><slot></slot></strong>
         </a>
-    </span>
-</p>`
+`
 
 export class CtoContactaWebComponent extends HTMLElement {
     constructor() {
