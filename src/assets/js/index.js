ddabe6c05c0ca6d88b5c4076b75b05195bf307ea
Ferran Llop <contacto@ferranllop.com>
2022-05-13 17:48:16 +0200

    Make services gallery to be a nunjucks partial instead of webcomponent to avoid unnecessary use of javascript.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,4 @@
 import {Review} from './Review.js'
-import {ServicesGallery} from './ServicesGallery.js'
 
 customElements.define('djm-review', Review)
-customElements.define('djm-services-gallery', ServicesGallery)
 
