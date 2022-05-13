f2db6ca22182aae707e4ee563bec3eba5875362b
Ferran Llop <contacto@ferranllop.com>
2022-05-13 17:20:18 +0200

    Make video to be a nunjucks partial instead of webcomponent to avoid unnecessary use of javascript.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,10 +1,8 @@
 import {Review} from './Review.js'
-import {VideoWebComponent} from './Video.js'
 import {CtoContactaWebComponent} from './CtoContactaWebComponent.js'
 import {ServicesGallery} from './ServicesGallery.js'
 
 customElements.define('djm-review', Review)
-customElements.define('djm-video', VideoWebComponent)
 customElements.define('djm-cto-contacta', CtoContactaWebComponent)
 customElements.define('djm-services-gallery', ServicesGallery)
 
