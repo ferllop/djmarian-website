75fa0e0f54e16aa32d7e7a16ea744697812da42d
Ferran Llop <contacto@ferranllop.com>
2022-05-13 17:41:41 +0200

    Make contact cto to be a nunjucks partial instead of webcomponent to avoid unnecessary use of javascript.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,6 @@
 import {Review} from './Review.js'
-import {CtoContactaWebComponent} from './CtoContactaWebComponent.js'
 import {ServicesGallery} from './ServicesGallery.js'
 
 customElements.define('djm-review', Review)
-customElements.define('djm-cto-contacta', CtoContactaWebComponent)
 customElements.define('djm-services-gallery', ServicesGallery)
 
