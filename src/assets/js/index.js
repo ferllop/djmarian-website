d2088bd488070db196d7bb33b99a36c091cada7d
Ferran Llop <contacto@ferranllop.com>
2022-05-13 17:03:46 +0200

    Make footer to be a nunjucks partial instead of webcomponent to avoid unnecessary use of js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,7 +7,6 @@ import {MainMenu} from './MainMenu.js'
 import {ServicesGallery} from './ServicesGallery.js'
 
 customElements.define('djm-main-header', MainHeader)
-customElements.define('djm-main-footer', MainFooter)
 customElements.define('djm-review', Review)
 customElements.define('djm-video', VideoWebComponent)
 customElements.define('djm-cto-contacta', CtoContactaWebComponent)
