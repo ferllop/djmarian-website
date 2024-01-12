26c0194482bf370396481647f39146ffd2959502
Ferran Llop <contacto@ferranllop.com>
2024-01-12 12:54:55 +0100

    Make year on footer to be a rendered by eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -12,6 +12,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     return {
         dir: {
