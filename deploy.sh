0f0a315f07a081c48bd0a8a381ad22978d13e9b6
Ferran Llop <contacto@ferranllop.com>
2024-01-19 14:36:37 +0100

    Change deploy to incorporate the possibility of deploying to local development environment

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+source .env
+cd docker
+
+print() {
+  echo -e "\n\n### $1 ###\n"
+}
+deployProduction(){
+  print "Deploying production"
+
+  print "Stopping previous server"
+  docker compose --env-file=../.env -f server.compose.yml -f ./traefik.server.compose.yml down
+
+  print "Installing dependencies if needed"
+  docker compose --env-file=../.env -f build.compose.yml run --rm node npm ci
+
+  print "Building"
+  docker compose --env-file=../.env -f build.compose.yml run --rm node npm run build
+
+  print "Starting server"
+  docker compose --env-file=../.env -f server.compose.yml -f ./traefik.server.compose.yml up -d
+}
+
+deployDevelopment(){
+  print "Stopping previous server"
+  docker compose --env-file=../.env -f server.compose.yml -f local.server.compose.yml down
+
+  print "Installing dependencies if needed"
+  docker compose --env-file=../.env -f build.compose.yml run --rm node npm install --no-save
+
+  print "Building"
+  docker compose --env-file=../.env -f build.compose.yml run --rm node npm run build
+
+  print "Starting server"
+  docker compose --env-file=../.env -f server.compose.yml -f local.server.compose.yml up -d
+
+  print "Serving on http://localhost:$LOCAL_PORT"
+}
+
+case $ENVIRONMENT in
+  production) deployProduction;;
+  development) deployDevelopment;;
+  *) echo "Please set the ENVIRONMENT environment variable in your .env file";exit 1;;
+esac
