170959a377df3941c18e0773c796a506f5ea96f4
Ferran Llop <contacto@ferranllop.com>
2024-02-02 20:08:00 +0100

    Import web components dynamically

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,12 @@
-import {Review} from './Review.js'
 import {Menu} from './Menu.js'
-import {Video} from './Video.js'
 
-customElements.define('djm-review', Review)
+async function defineIfPresent(path, tag, klass) {
+    if (document.querySelector(tag)) {
+        const file = await import(path)
+        customElements.define(tag, file[klass])
+    }
+}
+
+defineIfPresent('./Video.js', 'djm-video', 'Video') 
+defineIfPresent('./Review.js', 'djm-review', 'Review')
 customElements.define('djm-menu', Menu) 
-customElements.define('djm-video', Video) 
-
