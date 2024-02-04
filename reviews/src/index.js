ab255fcaaee1ed409963ccdbc2aa372e9d539382
Ferran Llop <contacto@ferranllop.com>
2024-02-04 18:25:17 +0100

    Make apache proxy reviews url to internal service

--- reviews/src/index.js
+++ reviews/src/index.js
@@ -2,7 +2,32 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Multiverse!'))
+const reviews = [
+	{
+		id: 0,
+		source: 'Google',
+		rating: 5,
+		name: 'First Review',
+		content: 'This is first review',
+	},
+	{
+		id: 1,
+		source: 'Google',
+		rating: 4,
+		name: 'Second Review',
+		content: 'This is second review',
+	},
+]
+
+app.get('/random', (req, res) => {
+	const index = 0
+	res.json(reviews[index])
+})
+
+app.get('/all', (req, res) => {
+	res.json(reviews)
+})
+
 
 const port = 3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Reviews service listening on port ${port}...\n`))
